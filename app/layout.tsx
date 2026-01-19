5d2e261cd60cb73ae29a3ce2749ff29cb614990b
Vladimir <vladimir@example.com>
2026-01-19 08:33:42 +0300

    Add mobile burger header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "./Header";
+import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "H2 Element",
